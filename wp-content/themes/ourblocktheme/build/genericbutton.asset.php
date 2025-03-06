d05dcd8f31ae2a03b35c95e0253e64d51b544ac6
Tyler Anderson <tanderson@oneupweb.com>
2025-03-06 13:50:11 -0500

    add banner component and color palette

---
+++ wp-content/themes/ourblocktheme/build/genericbutton.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-primitives'), 'version' => '98f6d7da7749ed0f4e9c');
---
+++ wp-content/themes/ourblocktheme/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '76a2c997832360ba1a07');
