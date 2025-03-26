cfdd548694ce5a6749c5ead1159ce1dbe5e47789
tyler <tander1001@gmail.com>
2025-03-25 21:35:05 -0400

    add Events and Blogs block with initial implementation and asset files

---
+++ wp-content/themes/ourblocktheme/build/eventsandblogs.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'b444cd41c5c3bcff0197');
---
+++ wp-content/themes/ourblocktheme/build/eventsandblogs.js
@@ -0,0 +1,27 @@
+/******/ (() => { // webpackBootstrap
+/*!**********************************!*\
+  !*** ./blocks/eventsandblogs.js ***!
+  \**********************************/
+wp.blocks.registerBlockType('ourblocktheme/eventsandblogs', {
+  title: 'Events and Blogs',
+  supports: {
+    align: ['full']
+  },
+  attributes: {
+    align: {
+      type: 'string',
+      default: 'full'
+    }
+  },
+  edit: function () {
+    return wp.element.createElement('div', {
+      className: 'our-placeholder-block'
+    }, 'This is Events and Blogs');
+  },
+  save: function () {
+    return null;
+  }
+});
+/******/ })()
+;
+//# sourceMappingURL=eventsandblogs.js.map
