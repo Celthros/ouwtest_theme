eb4dc2316b7b9d9e63da979c02c6e7bdf461c3a9
tyler <tander1001@gmail.com>
2025-04-01 18:27:26 -0400

    feat: add custom post type for events with necessary configurations

---
+++ wp-content/mu-plugins/university-post-types.php
@@ -0,0 +1,25 @@
+<?php
+
+function university_post_types(): void {
+	register_post_type( 'event',
+		array(
+			'labels'        => array(
+				'name'          => __( 'Events', 'ourblocktheme' ),
+				'singular_name' => __( 'Event', 'ourblocktheme' ),
+				'add_new_item'  => __( 'Add New Event', 'ourblocktheme' ),
+				'edit_item'     => __( 'Edit Event', 'ourblocktheme' ),
+				"all_items"     => __( 'All Events', 'ourblocktheme' ),
+			),
+			'public'        => true,
+			'has_archive'   => true,
+			'menu_icon'     => 'dashicons-calendar',
+			'menu_position' => 7,
+			'supports'      => array( 'title', 'editor', 'excerpt', 'thumbnail', 'custom-fields' ),
+			'rewrite'       => array( 'slug' => 'events' ),
+			'show_in_rest'  => true,
+
+		)
+	);
+}
+
+add_action( 'init', 'university_post_types' );
