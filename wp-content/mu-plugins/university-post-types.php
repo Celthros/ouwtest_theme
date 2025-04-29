8f3125fe8f65bf87beb7c4bbb1b3b80f275ca3d9
Celthros <tander1001@gmail.com>
2025-04-29 07:00:30 -0400

    feat: add campus post type and update event, program, and professor post types

--- wp-content/mu-plugins/university-post-types.php
+++ wp-content/mu-plugins/university-post-types.php
@@ -2,72 +2,93 @@
 
 function university_post_types(): void {
 
+	// Campus(s) Post Type
+	register_post_type( 'campus', array(
+		'labels'        => array(
+			'name'          => __( 'Campuses', 'ourblocktheme' ),
+			'singular_name' => __( 'Campus', 'ourblocktheme' ),
+			'add_new_item'  => __( 'Add New Campus', 'ourblocktheme' ),
+			'edit_item'     => __( 'Edit Campus', 'ourblocktheme' ),
+			'view_item'     => __( 'View Campus', 'ourblocktheme' ),
+			'view_items'    => __( 'View Campuses', 'ourblocktheme' ),
+			"all_items"     => __( 'All Campuses', 'ourblocktheme' ),
+		),
+		'public'        => true,
+		'has_archive'   => true,
+		'menu_icon'     => 'dashicons-location-alt',
+		'menu_position' => 7,
+		'supports'      => array( 'title', 'editor', 'excerpt', 'thumbnail', 'page-attributes' ),
+		'rewrite'       => array( 'slug' => 'campuses' ),
+		'show_in_rest'  => true,
+
+	) );
+
 	// Event(s) Post Type
 	register_post_type( 'event', array(
-			'labels'        => array(
-				'name'          => __( 'Events', 'ourblocktheme' ),
-				'singular_name' => __( 'Event', 'ourblocktheme' ),
-				'add_new_item'  => __( 'Add New Event', 'ourblocktheme' ),
-				'edit_item'     => __( 'Edit Event', 'ourblocktheme' ),
-				'view_item'     => __( 'View Event', 'ourblocktheme' ),
-				'view_items'    => __( 'View Events', 'ourblocktheme' ),
-				"all_items"     => __( 'All Events', 'ourblocktheme' ),
-			),
-			'public'        => true,
-			'has_archive'   => true,
-			'menu_icon'     => 'dashicons-calendar',
-			'menu_position' => 7,
-			'supports'      => array( 'title', 'editor', 'excerpt', 'thumbnail', 'page-attributes' ),
-			'rewrite'       => array( 'slug' => 'events' ),
-			'show_in_rest'  => true,
+		'labels'        => array(
+			'name'          => __( 'Events', 'ourblocktheme' ),
+			'singular_name' => __( 'Event', 'ourblocktheme' ),
+			'add_new_item'  => __( 'Add New Event', 'ourblocktheme' ),
+			'edit_item'     => __( 'Edit Event', 'ourblocktheme' ),
+			'view_item'     => __( 'View Event', 'ourblocktheme' ),
+			'view_items'    => __( 'View Events', 'ourblocktheme' ),
+			"all_items"     => __( 'All Events', 'ourblocktheme' ),
+		),
+		'public'        => true,
+		'has_archive'   => true,
+		'menu_icon'     => 'dashicons-calendar',
+		'menu_position' => 8,
+		'supports'      => array( 'title', 'editor', 'excerpt', 'thumbnail', 'page-attributes' ),
+		'rewrite'       => array( 'slug' => 'events' ),
+		'show_in_rest'  => true,
 
-		) );
+	) );
 
 
 	// Program(s) Post Type
 	register_post_type( 'program', array(
-			'labels'        => array(
-				'name'          => __( 'Programs', 'ourblocktheme' ),
-				'singular_name' => __( 'Program', 'ourblocktheme' ),
-				'add_new_item'  => __( 'Add New Program', 'ourblocktheme' ),
-				'edit_item'     => __( 'Edit Program', 'ourblocktheme' ),
-				'view_item'     => __( 'View Program', 'ourblocktheme' ),
-				'view_items'    => __( 'View Programs', 'ourblocktheme' ),
-				"all_items"     => __( 'All Programs', 'ourblocktheme' ),
+		'labels'        => array(
+			'name'          => __( 'Programs', 'ourblocktheme' ),
+			'singular_name' => __( 'Program', 'ourblocktheme' ),
+			'add_new_item'  => __( 'Add New Program', 'ourblocktheme' ),
+			'edit_item'     => __( 'Edit Program', 'ourblocktheme' ),
+			'view_item'     => __( 'View Program', 'ourblocktheme' ),
+			'view_items'    => __( 'View Programs', 'ourblocktheme' ),
+			"all_items"     => __( 'All Programs', 'ourblocktheme' ),
 
 
-			),
-			'public'        => true,
-			'has_archive'   => true,
-			'menu_icon'     => 'dashicons-awards',
-			'menu_position' => 8,
-			'supports'      => array( 'title', 'editor', 'thumbnail', 'page-attributes' ),
-			'rewrite'       => array( 'slug' => 'programs' ),
-			'show_in_rest'  => true,
+		),
+		'public'        => true,
+		'has_archive'   => true,
+		'menu_icon'     => 'dashicons-awards',
+		'menu_position' => 9,
+		'supports'      => array( 'title', 'editor', 'thumbnail', 'page-attributes' ),
+		'rewrite'       => array( 'slug' => 'programs' ),
+		'show_in_rest'  => true,
 
-		) );
+	) );
 
 	// Professor Post Type
 	register_post_type( 'professor', array(
-			'labels'        => array(
-				'name'          => __( 'Professors', 'ourblocktheme' ),
-				'singular_name' => __( 'Professor', 'ourblocktheme' ),
-				'add_new_item'  => __( 'Add New Professor', 'ourblocktheme' ),
-				'edit_item'     => __( 'Edit Professor', 'ourblocktheme' ),
-				'view_item'     => __( 'View Professor', 'ourblocktheme' ),
-				'view_items'    => __( 'View Professors', 'ourblocktheme' ),
-				"all_items"     => __( 'All Professors', 'ourblocktheme' ),
+		'labels'        => array(
+			'name'          => __( 'Professors', 'ourblocktheme' ),
+			'singular_name' => __( 'Professor', 'ourblocktheme' ),
+			'add_new_item'  => __( 'Add New Professor', 'ourblocktheme' ),
+			'edit_item'     => __( 'Edit Professor', 'ourblocktheme' ),
+			'view_item'     => __( 'View Professor', 'ourblocktheme' ),
+			'view_items'    => __( 'View Professors', 'ourblocktheme' ),
+			"all_items"     => __( 'All Professors', 'ourblocktheme' ),
 
 
-			),
-			'public'        => true,
-			'has_archive'   => false,
-			'menu_icon'     => 'dashicons-welcome-learn-more',
-			'menu_position' => 9,
-			'supports'      => array( 'title', 'editor', 'thumbnail', 'page-attributes' ),
-			'show_in_rest'  => true,
+		),
+		'public'        => true,
+		'has_archive'   => false,
+		'menu_icon'     => 'dashicons-welcome-learn-more',
+		'menu_position' => 10,
+		'supports'      => array( 'title', 'editor', 'thumbnail', 'page-attributes' ),
+		'show_in_rest'  => true,
 
-		) );
+	) );
 
 }
 
