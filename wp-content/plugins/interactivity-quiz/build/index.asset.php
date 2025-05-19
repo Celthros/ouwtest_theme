408f108e44856410f157d5ff47462cb1a431a456
Celthros <tander1001@gmail.com>
2025-05-19 07:13:33 -0400

    feat: added new plugin for api interaction

---
+++ wp-content/plugins/interactivity-quiz/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'd387de2ef051252b6fc8');
---
+++ wp-content/plugins/interactivity-quiz/build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => 'bcf2860b01de337686c0', 'type' => 'module');
