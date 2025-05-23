381496c17be4b6eed8694dfbcd8d84a88847a2ab
Celthros <tander1001@gmail.com>
2025-05-23 12:55:51 -0400

    feat: add custom REST fields and adjust queries for post types

--- wp-content/themes/ourblocktheme/controllers/Theme.php
+++ wp-content/themes/ourblocktheme/controllers/Theme.php
@@ -5,10 +5,11 @@ namespace Ourblocktheme\controllers;
 class Theme {
 
 	public function __construct() {
-		echo 'Theme instantiated!';
 		add_action( 'after_setup_theme', [ self::class, 'add_Theme_Support' ] );
 		add_filter( 'login_headerurl', [ self::class, 'ourHeaderUrl' ] );
 		add_filter( 'login_headertitle', [ self::class, 'ourLoginTitle' ] );
+		add_action( 'rest_api_init', [ self::class, 'university_custom_rest' ] );
+		add_action( 'pre_get_posts', [ self::class, 'university_adjust_queries' ] );
 	}
 
 	public static function add_Theme_Support(): void {
@@ -33,5 +34,78 @@ class Theme {
 	public static function ourLoginTitle(): null|string {
 		return get_bloginfo( 'name' );
 	}
+
+	public static function university_custom_rest(): void {
+		register_rest_field( 'post', 'authorName', array(
+			'get_callback' => function () {
+				return get_the_author();
+			},
+		) );
+
+		register_rest_field( 'note', 'userNoteCount', array(
+			'get_callback' => function () {
+				return count_user_posts( get_current_user_id(), 'note' );
+			},
+		) );
+	}
+
+	public static function pageBanner( $args = null ): void {
+
+		if ( ! isset( $args['title'] ) ) {
+			$args['title'] = get_the_title() ?? get_bloginfo( 'name' );
+		}
+
+		if ( ! isset( $args['subtitle'] ) ) {
+			$args['subtitle'] = get_field( 'page_banner_subtitle' ) ?? get_bloginfo( 'description' ) ?? 'Welcome to our university';
+		}
+
+		if ( ! isset( $args['photo'] ) ) {
+			$args['photo'] = get_theme_file_uri( '/images/ocean.jpg' ) ?? '';
+			if ( get_field( 'page_banner_background_image' ) && ! is_archive() && ! is_home() ) {
+				$args['photo'] = get_field( 'page_banner_background_image' )['sizes']['pageBanner'] ?? '';
+			}
+		}
+
+		?>
+        <div class="page-banner">
+            <div class="page-banner__bg-image" style="background-image: url(<?php echo $args['photo']; ?>);"></div>
+            <div class="page-banner__content container container--narrow">
+                <h1 class="page-banner__title"><?php echo $args['title'] ?></h1>
+                <div class="page-banner__intro">
+                    <p><?php echo $args['subtitle']; ?></p>
+                </div>
+            </div>
+        </div>
+	<?php }
+
+	public static function university_adjust_queries( $query ): void {
+
+		if ( ! is_admin() && is_post_type_archive( 'campus' ) && $query->is_main_query() ) {
+			$query->set( 'posts_per_page', - 1 );
+		}
+
+		if ( ! is_admin() && is_post_type_archive( 'program' ) && $query->is_main_query() ) {
+			$query->set( 'orderby', 'title' );
+			$query->set( 'order', 'ASC' );
+			$query->set( 'posts_per_page', - 1 );
+		}
+
+		if ( ! is_admin() && is_post_type_archive( 'event' ) && $query->is_main_query() ) {
+			$today = date( 'Ymd' );
+			$query->set( 'meta_key', 'event_date' );
+			$query->set( 'orderby', 'meta_value_num' );
+			$query->set( 'order', 'ASC' );
+			$query->set( 'meta_query', array(
+				array(
+					'key'     => 'event_date',
+					'compare' => '>=',
+					'value'   => $today,
+					'type'    => 'numeric',
+				),
+			) );
+		}
+	}
+
+
 
 }
--- wp-content/themes/ourblocktheme/controllers/ThemeEnqueue.php
+++ wp-content/themes/ourblocktheme/controllers/ThemeEnqueue.php
@@ -4,41 +4,43 @@ namespace Ourblocktheme\controllers;
 
 class ThemeEnqueue {
 
-	private static string $googleMapApiKey = "AIzaSyBRRnSwouBfAyhQ47rfDX0NMPcqiQ1Qm4s";
+	private static string $googleMapApiKey = "YOUR_GOOGLE_MAP_API_KEY"; // Replace with a secure source
 
 	public function __construct() {
 		self::load_dependencies();
-		add_action( 'wp_enqueue_scripts', [ self::class, 'theme_Enqueues' ] );
-		add_filter( 'login_enqueue_scripts', [ self::class, 'ourLoginCSS' ] );
-		add_filter( 'acf/fields/google_map/api', [ self::class, 'campusMapKey' ] );
+		$this->register_hooks();
 	}
 
-	public static function theme_Enqueues(): void {
+	private function register_hooks(): void {
+		add_action( 'wp_enqueue_scripts', [ self::class, 'themeEnqueues' ] );
+		add_filter( 'login_enqueue_scripts', [ self::class, 'ourLoginCSS' ] );
+		add_filter( 'acf/fields/google_map/api', [ self::class, 'campusMapKey' ] );
+		add_action( 'init', [ self::class, 'our_new_blocks' ] );
+	}
+
+	public static function themeEnqueues(): void {
 		wp_enqueue_script( 'googleMap', '//maps.googleapis.com/maps/api/js?key=' . self::$googleMapApiKey . '&v=weekly&libraries=marker', null, '1.0', true );
-		wp_enqueue_script( 'main-university-js', get_theme_file_uri( '/build/index.js' ), array( 'jquery' ), '1.0', true );
+		wp_enqueue_script( 'main-university-js', get_theme_file_uri( '/build/index.js' ), [ 'jquery' ], '1.0', true );
 		wp_enqueue_style( 'custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i' );
 		wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' );
 		wp_enqueue_style( 'university_main_styles', get_theme_file_uri( '/build/style-index.css' ) );
 		wp_enqueue_style( 'university_extra_styles', get_theme_file_uri( '/build/index.css' ) );
 
-		wp_localize_script( 'main-university-js', 'universityData', array(
+		wp_localize_script( 'main-university-js', 'universityData', [
 			'root_url' => get_site_url(),
 			'nonce'    => wp_create_nonce( 'wp_rest' ),
-		) );
-
+		] );
 	}
 
 	public static function ourLoginCSS(): void {
-		wp_enqueue_style( 'custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i' );
-		wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' );
-		wp_enqueue_style( 'university_main_styles', get_theme_file_uri( '/build/style-index.css' ) );
-		wp_enqueue_style( 'university_extra_styles', get_theme_file_uri( '/build/index.css' ) );
+		// Reuse styles from themeEnqueues to avoid duplication
+		self::themeEnqueues();
 	}
 
 	private static function load_dependencies(): void {
-		require get_theme_file_path('/inc/like-route.php');
-		require get_theme_file_path('/inc/search-route.php');
-		require get_theme_file_path('/inc/gtm.php');
+		require get_theme_file_path( '/inc/like-route.php' );
+		require get_theme_file_path( '/inc/search-route.php' );
+		require get_theme_file_path( '/inc/gtm.php' );
 	}
 
 	public static function campusMapKey( $api ): array {
@@ -47,4 +49,37 @@ class ThemeEnqueue {
 		return $api;
 	}
 
+	public static function our_new_blocks(): void {
+		wp_localize_script( 'wp-editor', 'ourThemeData', [ 'themePath' => get_stylesheet_directory_uri() ] );
+
+		$ourBlocks = [
+			"genericheading",
+			"genericbutton",
+			"slide",
+			"slideshow",
+			"page",
+			"blogindex",
+			"programarchive",
+			"archivecampus",
+			"archive-event",
+			"search",
+			"searchresults",
+			"singlecampus",
+			"singleevent",
+			"singleprofessor",
+			"singleprogram",
+			"pastevents",
+			"mynotes",
+			"singlepost",
+			"archive",
+			"banner",
+			"footer",
+			"header",
+			"eventsandblogs"
+		];
+
+		foreach ( $ourBlocks as $ourBlock ) {
+			register_block_type_from_metadata( __DIR__ . "/build/" . $ourBlock );
+		}
+	}
 }
