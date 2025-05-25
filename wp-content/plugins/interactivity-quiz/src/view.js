5ceff28e4ef0de1b6b1b132e88b7b7cd3a2fa3c7
Celthros <tander1001@gmail.com>
2025-05-24 22:27:34 -0400

    refactor: improve code formatting and consistency in view.js

--- wp-content/plugins/interactivity-quiz/src/view.js
+++ wp-content/plugins/interactivity-quiz/src/view.js
@@ -1,44 +1,44 @@
 /**
  * WordPress dependencies
  */
-import { store, getContext } from '@wordpress/interactivity';
+import { store , getContext } from '@wordpress/interactivity';
 
-const { state } = store('create-block', {
-	state: {
-		get themeText() {
+const { state } = store ( 'create-block' , {
+	state : {
+		get themeText () {
 			return state.isDark ? state.darkText : state.lightText;
-		},
-	},
-	actions: {
-		buttonHandler: () => {
-			const context = getContext();
-			if (context && typeof context.clickCount === 'number') {
-				context.clickCount++;
-				console.log('Button was clicked!');
+		} ,
+	} ,
+	actions : {
+		buttonHandler : () => {
+			const context = getContext ();
+			if ( context && typeof context.clickCount === 'number' ) {
+				context.clickCount ++;
+				console.log ( 'Button was clicked!' );
 			} else {
-				console.error('Invalid context or clickCount is not a number.');
+				console.error ( 'Invalid context or clickCount is not a number.' );
 			}
-		},
-		toggleOpen() {
-			const context = getContext();
-			if (context) {
-				context.isOpen = !context.isOpen;
+		} ,
+		toggleOpen () {
+			const context = getContext ();
+			if ( context ) {
+				context.isOpen = ! context.isOpen;
 			} else {
-				console.error('Context is undefined.');
+				console.error ( 'Context is undefined.' );
 			}
-		},
-		toggleTheme() {
-			state.isDark = !state.isDark;
-		},
-	},
-	callbacks: {
-		logIsOpen: () => {
-			const context = getContext();
-			if (context) {
-				console.log(`Is open: ${context.isOpen}`);
+		} ,
+		toggleTheme () {
+			state.isDark = ! state.isDark;
+		} ,
+	} ,
+	callbacks : {
+		logIsOpen : () => {
+			const context = getContext ();
+			if ( context ) {
+				console.log ( `Is open: ${ context.isOpen }` );
 			} else {
-				console.error('Context is undefined.');
+				console.error ( 'Context is undefined.' );
 			}
-		},
-	},
-});
+		} ,
+	} ,
+} );
