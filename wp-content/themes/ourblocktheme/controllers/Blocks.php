bfdb4e360e85e75c1a5872541c80c3371ffc69c2
Celthros <tander1001@gmail.com>
2025-05-31 20:30:52 -0400

    feat: implement autoLoadBlocks function to register blocks from directory

--- wp-content/themes/ourblocktheme/controllers/Blocks.php
+++ wp-content/themes/ourblocktheme/controllers/Blocks.php
@@ -6,9 +6,22 @@ class Blocks {
 
 	public function __construct() {
 		add_action( 'init', [ self::class, 'enqueueBlocks' ] );
+		//add_action( 'init', [ self::class, 'autoLoadBlocks' ] );
 	}
 
-	public static function registerBlocks() {
+	public static function autoLoadBlocks(): void {
+		$blockPath         = get_template_directory() . "/build/blocks/";
+		$directoryIterator = new \RecursiveIteratorIterator( new \RecursiveDirectoryIterator( $blockPath ) );
+
+		wp_localize_script( 'wp-editor', 'ourThemeData', [ 'themePath' => get_stylesheet_directory_uri() ] );
+
+		foreach ( $directoryIterator as $file ) {
+			if ( $file->isFile() && $file->getFilename() === 'block.json' ) {
+				$blockDir = $file->getPath();
+				var_dump( $blockDir );
+				register_block_type_from_metadata( $blockDir );
+			}
+		}
 	}
 
 	public static function enqueueBlocks(): void {
