5732bce6fca49d9355061ad8c83d2c0c1fdf9e2d
Celthros <tander1001@gmail.com>
2025-06-04 21:06:11 -0400

    style: format render.php for consistent indentation and spacing

--- wp-content/themes/ourblocktheme/src/programarchive/render.php
+++ wp-content/themes/ourblocktheme/src/programarchive/render.php
@@ -1,24 +1,23 @@
 <?php
 
-pageBanner(array(
-	'title' => 'All Programs',
+pageBanner( array(
+	'title'    => 'All Programs',
 	'subtitle' => 'There is something for everyone. Have a look around.'
-));
+) );
 ?>
 
 <div class="container container--narrow page-section">
 
-	<ul class="link-list min-list">
+    <ul class="link-list min-list">
 
 		<?php
-		while(have_posts()) {
+		while ( have_posts() ) {
 			the_post(); ?>
-			<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+            <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
 		<?php }
 		echo paginate_links();
 		?>
-	</ul>
-
+    </ul>
 
 
 </div>
--- wp-content/themes/ourblocktheme/src/singleprofessor/render.php
+++ wp-content/themes/ourblocktheme/src/singleprofessor/render.php
@@ -6,14 +6,14 @@ pageBanner();
 
 <div class="container container--narrow page-section">
 
-	<div class="generic-content">
-		<div class="row group">
+    <div class="generic-content">
+        <div class="row group">
 
-			<div class="one-third">
+            <div class="one-third">
 				<?php the_post_thumbnail( 'professorPortrait' ); ?>
-			</div>
+            </div>
 
-			<div class="two-thirds">
+            <div class="two-thirds">
 				<?php
 
 				$likeCount = new WP_Query( array(
@@ -50,7 +50,7 @@ pageBanner();
 
 				?>
 
-				<span class="like-box" data-like="<?php if ( isset( $existQuery->posts[0]->ID ) ) {
+                <span class="like-box" data-like="<?php if ( isset( $existQuery->posts[0]->ID ) ) {
 					echo $existQuery->posts[0]->ID;
 				} ?>" data-professor="<?php the_ID(); ?>" data-exists="<?php echo $existStatus; ?>">
               <i class="fa fa-heart-o" aria-hidden="true"></i>
@@ -58,23 +58,24 @@ pageBanner();
               <span class="like-count"><?php echo $likeCount->found_posts; ?></span>
             </span>
 				<?php the_content(); ?>
-			</div>
+            </div>
 
-		</div>
-	</div>
+        </div>
+    </div>
 
 	<?php
 
 	$relatedPrograms = get_field( 'related_programs' );
 
 	if ( $relatedPrograms ) { ?>
-		<hr class="section-break">
-		<h2 class="headline headline--medium">Subject(s) Taught</h2>
-		<ul class="link-list min-list">
+        <hr class="section-break">
+        <h2 class="headline headline--medium">Subject(s) Taught</h2>
+        <ul class="link-list min-list">
 			<?php foreach ( $relatedPrograms as $program ) : ?>
-				<li><a href="<?php echo get_the_permalink( $program ); ?>"><?php echo get_the_title( $program ); ?></a></li>
+                <li><a href="<?php echo get_the_permalink( $program ); ?>"><?php echo get_the_title( $program ); ?></a>
+                </li>
 			<?php endforeach; ?>
-		</ul>
+        </ul>
 	<?php } ?>
 
 </div>
