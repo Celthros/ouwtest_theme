3f94312f42825dff2a5e35841d199251ee2a608c
Celthros <tander1001@gmail.com>
2025-06-11 07:03:06 -0400

    feat: update block.json defaults to empty and enhance render logic for title, subtitle, and photo attributes

--- wp-content/themes/ourblocktheme/src/pagebanner/render.php
+++ wp-content/themes/ourblocktheme/src/pagebanner/render.php
@@ -1,16 +1,20 @@
 <?php
 
-if ( ! isset( $args['title'] ) ) {
+$args['title'] = $attributes['title'];
+$args['subtitle'] = $attributes['subtitle'];
+$args['photo'] = $attributes['photo'];
+
+if ( empty( $args['title'] ) ) {
 	$args['title'] = esc_html( get_the_title() ?? get_bloginfo( 'name' ) );
 }
 
-if ( ! isset( $args['subtitle'] ) ) {
+if ( empty( $args['subtitle'] ) ) {
 	$args['subtitle'] = function_exists( 'get_field' )
 		? esc_html( get_field( 'page_banner_subtitle' ) ?? get_bloginfo( 'description' ) ?? 'Welcome to our university' )
 		: esc_html( get_bloginfo( 'description' ) ?? 'Welcome to our university' );
 }
 
-if ( ! isset( $args['photo'] ) ) {
+if ( empty( $args['photo'] ) ) {
 	$args['photo'] = esc_url( get_theme_file_uri( '/images/ocean.jpg' ) );
 	if ( function_exists( 'get_field' ) && get_field( 'page_banner_background_image' ) && ! is_archive() && ! is_home() ) {
 		$args['photo'] = esc_url( get_field( 'page_banner_background_image' )['sizes']['pageBanner'] ?? get_theme_file_uri( '/images/ocean.jpg' ) );
